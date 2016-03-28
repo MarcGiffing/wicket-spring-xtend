ef5c64a8f356cb3f2ce6cf829c4b243862d77285
Marc Giffing <marcgiffing@web.de>
2016-03-28 20:50:15 +0200

    removed xtext-gen folder

--- wicket-spring-xtend-activeannotations/src/main/xtend-gen/com/giffing/wicket/spring/xtend/activeannotations/Join.java
+++
@@ -1,9 +0,0 @@
-package com.giffing.wicket.spring.xtend.activeannotations;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.TYPE)
-public @interface Join {
-  public Class value();
-}
